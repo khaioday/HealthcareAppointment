40a0c811eb686704c10d83cb3a07b9468e553c5b
minhkhai10122003 <136252548+khaioday@users.noreply.github.com>
2024-10-13 18:45:05 +0700

    Create IAppointmentRepository.cs

---
+++ HealthcareAppointment.Business/IAppointmentRepository.cs
@@ -0,0 +1,16 @@
+﻿using HealthcareAppointment.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HealthcareAppointment.Data
+{
+    public interface IAppointmentRepository
+    {
+        Task<IEnumerable<Appointment>> GetAppointmentsAsync();           // Get all appointments
+        Task<Appointment> GetAppointmentByIdAsync(Guid id);            // Get appointment by ID
+        Task AddAppointmentAsync(Appointment appointment);              // Add a new appointment
+        Task UpdateAppointmentAsync(Appointment appointment);           // Update an existing appointment
+        Task DeleteAppointmentAsync(Guid id);                           // Delete an appointment by ID
+    }
+}
