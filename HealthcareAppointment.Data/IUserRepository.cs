2408e70f702764e38010a04f0394b9ac6dce4aab
minhkhai10122003 <136252548+khaioday@users.noreply.github.com>
2024-10-13 18:45:15 +0700

    Create IUserRepository.cs

---
+++ HealthcareAppointment.Data/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using HealthcareAppointment.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HealthcareAppointment.Data
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetUsersAsync();
+        Task<User> GetUserByIdAsync(Guid id);
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(Guid id);
+    }
+}
