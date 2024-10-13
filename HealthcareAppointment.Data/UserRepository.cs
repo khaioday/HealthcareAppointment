6d9320386c2cff952703eac956383acc27e52375
minhkhai10122003 <136252548+khaioday@users.noreply.github.com>
2024-10-13 18:45:18 +0700

    Create UserRepository.cs

---
+++ HealthcareAppointment.Data/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using HealthcareAppointment.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HealthcareAppointment.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly HealthcareAppointmentDbContext _context;
+
+        public UserRepository(HealthcareAppointmentDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(Guid id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteUserAsync(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
