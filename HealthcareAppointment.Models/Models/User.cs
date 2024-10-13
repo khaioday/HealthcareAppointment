c2515f610d1a5b47ed6f41a86f2bc82317269833
minhkhai10122003 <136252548+khaioday@users.noreply.github.com>
2024-10-13 17:51:50 +0700

    Update User.cs

--- HealthcareAppointment.Models/Models/User.cs
+++ HealthcareAppointment.Models/Models/User.cs
@@ -7,25 +7,17 @@ namespace HealthcareAppointment.Models.Models;
 
 public partial class User
 {
-    public int Id { get; set; }
+    public string Id { get; set; }
 
-    public string Username { get; set; }
-
-    public string Password { get; set; }
-
-    public string Nickname { get; set; }
+    public string Name { get; set; }
 
     public string Email { get; set; }
 
-    public string PhoneNumber { get; set; }
+    public DateOnly? DateOfBirth { get; set; }
 
-    public string Address { get; set; }
+    public string Password { get; set; }
 
-    public int? RoleId { get; set; }
+    public string Role { get; set; }
 
-    public string Image { get; set; }
-
-    public virtual ICollection<Feedback> Feedbacks { get; set; } = new List<Feedback>();
-
-    public virtual Role Role { get; set; }
+    public string Specialization { get; set; }
 }
